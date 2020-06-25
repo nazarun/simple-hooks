28ebe353e96540a481b37c7ae6570b2f1ae56cb0
Nazar Krukovsky <nazarun@gmail.com>
2020-06-25 11:33:36 +0300

    Modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import ResourceList from './ResourceList';
 import UserList from './UserList';
+import Modal from './Modal';
+import useModal from './useModal';
+
 
 const App = () => {
     const [resource, setResource] = useState('posts');
+    const {isShowing, toggle} = useModal();
 
     return (
         <div>
             <UserList />
+            <button className="button-default" onClick={toggle}>Show Modal</button>
             <div>
                 <button onClick={() => setResource('posts')}>Posts</button>
                 <button onClick={() => setResource('todos')}>Todos</button>
             </div>
             <ResourceList resource={resource}/>
+            <Modal
+                isShowing={isShowing}
+                hide={toggle}
+            />
         </div>
     );
 
